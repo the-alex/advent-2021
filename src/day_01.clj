c3236c8a8c7eeb9b19e87e3f2b56aa4200b59ca1
Alex Chojnacki <thealex@umich.edu>
2021-12-01 22:54:24 -0500

    refactor: Such a nice cleanup of day 1

--- src/day_01.clj
+++ src/day_01.clj
@@ -1,49 +1,42 @@
 (ns day-01
-  "It's a new dawn;
-  a new day.
-  A new life."
+  "A new dawn; a new day. A new life."
   (:require [clojure.java.io :as io]
             [cuerdas.core :as str]
-            [wing.core :as w]
             [clojure.edn :as edn]))
 
-(def ex-in
-  "199
-200
-208
-210
-200
-207
-240
-269
-260
-263")
-
-(defn parse-input [in]
+(defn parse-input
+  "Read the input string, a newline delimited list of integers."
+  [in]
   (->> in
        str/split
        (map #(edn/read-string %))))
 
-(defn p1 [input]
-  (->> input
+(defn count-ascending
+  "How often does our sonar scan a greater depth than the previous measure?"
+  [measures]
+  (->> measures
        (partition 2 1)
-       (map #(apply < %))
+       (map (partial apply <))
        (filter true?)
        count))
 
-(defn p2 [input]
+(defn p1 [input] (count-ascending input))
+
+(defn p2
+  "Let's use the average of a sliding window to smooth our measurements."
+  [input {:keys [window-size]
+          :or {window-size 2}
+          :as opts}]
   (->> input
-       (partition 3 1)
+       (partition window-size 1)
        (map (partial apply +))
-       (partition 2 1)
-       (map #(apply < %))
-       (filter true?)
-       count))
+       count-ascending))
 
-(let [input (-> #_ex-in
-                (slurp (io/resource "day_01_input.txt"))
-                parse-input)
-      p1? (p1 input)
-      p2? (p2 input)]
+(comment
+  (let [input (-> "day_01_input.txt"
+                  io/resource
+                  slurp
+                  parse-input)]
 
-  (w/make-map p1? p2?))
+    {:p1? (p1 input)
+     :p2? (p2 input {:window-size 3})}))
